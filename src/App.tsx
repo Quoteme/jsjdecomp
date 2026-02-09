50179b446f903bb7077a39291542116e3b07f4fe
Luca Leon Happel <lucahappel99@gmx.de>
2026-02-09 12:21:47 +0100

    add: path-tracing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { OrbitControls } from "@react-three/drei";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Torus } from "./Torus";
 import range from "rrjx";
-import { add } from "mathjs";
 import { Color, TextureLoader, Vector3, WebGLRenderer } from "three";
 import { useLoader } from "@react-three/fiber";
 import { Leva, useControls } from "leva";
 import toast, { Toaster } from "react-hot-toast";
+import { Pathtracer } from "@react-three/gpu-pathtracer";
+
 function App() {
   const glRef = useRef<WebGLRenderer>(null);
-  const [count, setCount] = useState(0);
 
   const {
     splitting_num,
@@ -24,6 +22,7 @@ function App() {
     jsj_tori_logitude_compressing_disk,
     jsj_tori_meridian_compressing_disk,
     jsj_obstruction_opacity,
+    path_tracing,
   } = useControls({
     splitting_num: {
       value: 4,
@@ -81,6 +80,10 @@ function App() {
       step: 0.01,
       hint: "Opacity of the obstructions in the toroidal splitting",
     },
+    path_tracing: {
+      value: false,
+      hint: "Whether to enable path tracing or not. Enabling this will significantly increase the rendering time, but will produce more accurate and visually appealing results.",
+    },
   });
   const offset: Vector3 = new Vector3(3.5, 0, 0);
 
@@ -92,36 +95,25 @@ function App() {
         <group
           position={offset.clone().multiplyScalar(i - (splitting_num - 1) / 2)}
         >
-          <mesh
-            key={`jsj_torus_${i}`}
-            rotation={[Math.PI / 2, 0, 0]}
-            renderOrder={1}
-          >
+          <mesh key={`jsj_torus_${i}`} rotation={[Math.PI / 2, 0, 0]}>
             <Torus R={6 / 5} r={1 / 3} />
             <meshPhongMaterial
               map={texture_globe}
               opacity={jsj_tori_opacity}
               transparent={true}
-              depthWrite={false}
             />
           </mesh>
-          <mesh
-            key={`jsj_torus_logitude_${i}`}
-            rotation={[Math.PI / 2, 0, 0]}
-            renderOrder={-1}
-          >
+          <mesh key={`jsj_torus_logitude_${i}`} rotation={[Math.PI / 2, 0, 0]}>
             <Torus R={8 / 5} r={0.075} />
             <meshPhongMaterial
               color={[1, 0, 0]}
               opacity={jsj_tori_logitude}
               transparent={true}
-              depthWrite={false}
             />
           </mesh>
           <mesh
             key={`jsj_torus_logitude_compressing_disk_${i}`}
             rotation={[Math.PI / 2, 0, 0]}
-            renderOrder={-1}
           >
             <circleGeometry args={[8 / 5 - 0.075, 64]} />
             <meshPhongMaterial
@@ -129,28 +121,24 @@ function App() {
               opacity={jsj_tori_logitude_compressing_disk}
               transparent={true}
               side={2}
-              depthWrite={false}
             />
           </mesh>
           <mesh
             key={`jsj_torus_meridian_${i}`}
             position={new Vector3(0, 0, 6 / 5)}
             rotation={[0, Math.PI / 2, 0]}
-            renderOrder={-1}
           >
             <Torus R={2 / 5} r={0.075} />
             <meshPhongMaterial
               color={[0, 0, 1]}
               opacity={jsj_tori_meridian}
               transparent={true}
-              depthWrite={false}
             />
           </mesh>
           <mesh
             key={`jsj_torus_meridian_compressing_disk_${i}`}
             position={new Vector3(0, 0, 6 / 5)}
             rotation={[0, Math.PI / 2, 0]}
-            renderOrder={-1}
           >
             <circleGeometry args={[2 / 5 - 0.075, 64]} />
             <meshPhongMaterial
@@ -158,47 +146,37 @@ function App() {
               opacity={jsj_tori_meridian_compressing_disk}
               transparent={true}
               side={2}
-              depthWrite={false}
             />
           </mesh>
-          <mesh
-            key={`jsj_component_${i}`}
-            rotation={[Math.PI / 2, 0, 0]}
-            renderOrder={0}
-          >
+          <mesh key={`jsj_component_${i}`} rotation={[Math.PI / 2, 0, 0]}>
             <Torus R={6 / 5} r={1 / 4} />
             <meshPhongMaterial
               color={new Color().setHSL(i / splitting_num, 1, 0.5)}
               opacity={jsj_components_opacity}
               transparent={true}
-              depthWrite={false}
             />
           </mesh>
           <mesh
             key={`jsj_obsturction_longitude${i}`}
             position={new Vector3(0, 0, 6 / 5)}
             rotation={[0, Math.PI / 2, 0]}
-            renderOrder={0}
           >
             <Torus R={4 / 5} r={0.15} />
             <meshPhongMaterial
               color={new Color().setHSL(0, 0, 0.5)}
               opacity={jsj_obstruction_opacity}
               transparent={true}
-              depthWrite={false}
             />
           </mesh>
           <mesh
             key={`jsj_obsturction_meridian${i}`}
             rotation={[Math.PI / 2, 0, 0]}
-            renderOrder={0}
           >
             <Torus R={6 / 5} r={0.15} />
             <meshPhongMaterial
               color={new Color().setHSL(0, 0, 0.5)}
               opacity={jsj_obstruction_opacity}
               transparent={true}
-              depthWrite={false}
             />
           </mesh>
         </group>
@@ -211,7 +189,12 @@ function App() {
       <Leva />
       <Canvas
         camera={{ position: [0, 5, 9], fov: 50 }}
-        gl={{ antialias: true, alpha: true, preserveDrawingBuffer: true }}
+        gl={{
+          antialias: true,
+          alpha: true,
+          preserveDrawingBuffer: true,
+          premultipliedAlpha: true,
+        }}
         onCreated={({ gl }) => {
           gl.setClearColor(0x000000, 0);
           glRef.current = gl;
@@ -232,10 +215,12 @@ function App() {
           );
         }}
       >
-        <OrbitControls />
-        <ambientLight intensity={0.2} />
-        <directionalLight position={[8, 8, 2]} intensity={1} castShadow />
-        {jsj_tori}
+        <Pathtracer enabled={path_tracing}>
+          <OrbitControls makeDefault enableDamping={false} />
+          <ambientLight intensity={0.2} />
+          <directionalLight position={[8, 8, 2]} intensity={1} castShadow />
+          {jsj_tori}
+        </Pathtracer>
       </Canvas>
       <Toaster position="top-right" />
     </>
